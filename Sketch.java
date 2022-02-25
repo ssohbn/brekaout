119079e8b00be52f7167b48bc00c43f474b2460a
me <me>
2022-02-25 09:02:59 -0800

    awef

--- Sketch.java
+++ Sketch.java
@@ -19,18 +19,22 @@ public class Sketch extends PApplet {
 
   public void draw() {
     background(10);
+
     if (player.getLives() != 0) {
       if (!player.hasClicked) {
         text("hit the mouse button to begin", 80, 300);  // Default depth, no z-value specified
       }
 
+      fill(255,0,0);
       player.draw(this);
       player.update(this);
 
       drawLives();
       drawBricks();
+
       GameLogic.checkCollides(ball, this);
       
+      fill(255,0,0);
       if (player.hasClicked) {
         ball.draw(this);
         ball.update(this);
